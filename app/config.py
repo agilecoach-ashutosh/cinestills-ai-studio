6691b343e3c84f3644a4aefa1a28066b2c057961
Ashutosh Mishra <agilecoach.ashutosh@gmail.com>
2026-09-21 19:11:56 +0530

    Add app/config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class AppConfig:
+    invoke_base_url: str = "http://127.0.0.1:9090"
+    app_name: str = "CineStills AI Studio"
+    version: str = "0.1.0"
+
+CONFIG = AppConfig()
