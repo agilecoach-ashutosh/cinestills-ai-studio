89e883dc45d97a0cd38f2c91d5adcb66ec62f739
Ashutosh Mishra <agilecoach.ashutosh@gmail.com>
2026-09-22 18:35:34 +0530

    Bump CineStills to v0.3.0

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 class AppConfig:
     invoke_base_url: str = "http://127.0.0.1:9090"
     app_name: str = "CineStills AI Studio"
-    version: str = "0.2.0"
+    version: str = "0.3.0"
 
 
 CONFIG = AppConfig()
